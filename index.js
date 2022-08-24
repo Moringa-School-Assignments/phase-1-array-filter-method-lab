a8ead9bc158f4c59b0ca7878ee1000bfd82b557c
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-08-25 02:55:47 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter(name => name.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(name => name.startsWith(string))
+}
+
+function matchName(array, string) {
+    return array.filter(obj => obj.name === string)
+}
